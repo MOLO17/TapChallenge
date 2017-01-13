4f5b43069d773d0b8432367cc4e2a099cb2c3891
Daniele Angeli <daniele.angeli@molo17.com>
2017-01-13 16:14:47 +0100

    initial commit

---
+++ TapChallange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapChallange
+//
+//  Created by Daniele Angeli on 13/01/17.
+//  Copyright © 2017 MOLO17 Srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TapChallange/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TapChallange
+//
+//  Created by Daniele Angeli on 13/01/17.
+//  Copyright © 2017 MOLO17 Srl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
